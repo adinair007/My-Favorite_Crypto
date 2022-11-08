cdf82d3fc49d17d44c1fffc5a1cf4d02d4eed51b
Aditya Nair <adinair.in@gmail.com>
2022-11-07 20:35:46 -0600

    going with cryptocompare api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,27 @@
-var marketList = document.getElementById("current-market");
+var marketListEl = document.getElementById("current-market");
 
-// const CoinGecko = require("coingecko-api");
-// const coinGeckoClient = new CoinGecko();
-
-// export async function getList(context) {
-//   const params = {
-//     order: CoinGecko.ORDER.MARKET_CAP_DESC,
-//   };
-
-//   const result = await coinGeckoClient.coins.markets({ params });
-// }
-
-var liveMarket = {
-  async: true,
-  scroosDomain: true,
-  url:"https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=5cce2c9f-ec33-4178-81dc-c4295870dc99",
-  method: "GET",
-  headers: {},
+var liveMarket = function () {
+  var apiURL =
+    "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR&api_key={7a0dd8d530d2272b9577a48c3a0653fe13dea219bd9210b461767355f5c12272}";
+  fetch(apiURL).then(function (response) {
+    console.log(response);
+    response.json().then(function (data) {
+    currentMarket(data);
+    });
+  });
 };
+liveMarket();
 
-$.ajaxStart(liveMarket).done(function (response) {
-  console.log(response);
-  marketList.innerHTML = response.bitcoin;
-  marketList.innerHTML = response.ethereum;
-});
+var currentMarket = function(market) {
+  marketListEl.textContent = "";
 
-// var liveMarket = function (event) {
-//   var apiURL =
-//     "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=5cce2c9f-ec33-4178-81dc-c4295870dc99";
-//   fetch(apiURL).then(function (response) {
-//     console.log(response);
-//     response.json().then(function (data) {
-//       marketList(data, event);
-//     });
-//   });
-// };
+  var cryptoIconEl = document.createElement("span");
+  var cryptoIcon = document.createElement("img");
+  cryptoIcon.setAttribute("src",`/media/37746251/btc.png`)
+
+
+  var bitcoinEl = document.createElement("span");
+  bitcoinEl.textContent = "Bitcoin: $" + market.overview;
+  bitcoinEl.classList = "list-group-item";
+
+}
