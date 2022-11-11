c61a784e73bbfafe48b059a139f4773b37a956d7
Aditya Nair <adinair.in@gmail.com>
2022-11-11 15:38:14 -0600

    Added css elements to crypto-table

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,6 @@
 var marketListEl = document.getElementById("market-table");
 var topFiveEl = document.getElementById("topFive-list");
 
-
-
-
-
-
 var liveMarket = function () {
   var apiURL =
     "https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD&api_key=7a0dd8d530d2272b9577a48c3a0653fe13dea219bd9210b461767355f5c12272";
@@ -18,12 +13,23 @@ var liveMarket = function () {
 };
 liveMarket();
 
-
-
 var currentMarket = function (market) {
   marketListEl.textContent = "";
-  let headers = ["#", "Coin", "Price", "Market Cap", "24h Change", "24h High"];
-  var table = document.createElement("table");
+  let headers = [
+    "#",
+    "Symbol",
+    "Coin Name",
+    "Price",
+    "Market Cap",
+    "24h Change",
+    "24h High",
+  ];
+  
+  var cryptoTable = document.createElement("Table");
+  cryptoTable.classList.add("hover");
+  cryptoTable.style.background = "white";
+  cryptoTable.style.textAlign = "center";
+
   var headerRow = document.createElement("tr");
 
   headers.forEach((headerText) => {
@@ -32,29 +38,37 @@ var currentMarket = function (market) {
     header.appendChild(textNode);
     headerRow.appendChild(header);
   });
-  table.appendChild(headerRow);
-  marketListEl.appendChild(table);
+  cryptoTable.appendChild(headerRow);
+  marketListEl.appendChild(cryptoTable);
 
   var dataListEL = market.Data;
+
   for (var i = 0; i < dataListEL.length; i++) {
-    
     var cryptoList = dataListEL[i];
-    
+
     var cryptoData = document.createElement("tr");
 
+    // var cryptoIndex = document.createElement("td");
+    // cryptoIndex.textContent= Array.indexOf(cryptoList,0);
+    // cryptoData.appendChild(cryptoIndex);
+
+    
     var cryptoSymbol = document.createElement("td");
     var cryptoIcon = document.createElement("img");
-    cryptoIcon.setAttribute("src", `https://www.cryptocompare.com/${cryptoList.CoinInfo.ImageUrl}`);
-    cryptoIcon.style.width = '35px';
-    cryptoIcon.style.height = 'auto';
+    cryptoIcon.setAttribute(
+      "src",
+      `https://www.cryptocompare.com/${cryptoList.CoinInfo.ImageUrl}`
+    );
+    cryptoIcon.style.width = "35px";
+    cryptoIcon.style.height = "auto";
     cryptoSymbol.appendChild(cryptoIcon);
     cryptoData.appendChild(cryptoSymbol);
-    
+
     var cryptoName = document.createElement("td");
-    cryptoName.textContent = cryptoList.CoinInfo.FullName + "\n" + cryptoList.CoinInfo.Name;    
+    cryptoName.textContent =
+      cryptoList.CoinInfo.FullName + "\n" + cryptoList.CoinInfo.Name;
     cryptoData.appendChild(cryptoName);
 
-  
     var cryptoPrice = document.createElement("td");
     cryptoPrice.textContent = cryptoList.DISPLAY.USD.PRICE;
     cryptoData.appendChild(cryptoPrice);
@@ -71,8 +85,24 @@ var currentMarket = function (market) {
     dayHigh.textContent = cryptoList.DISPLAY.USD.HIGH24HOUR;
     cryptoData.appendChild(dayHigh);
 
-    table.appendChild(cryptoData);
+    var cryptoBtns = document.createElement("td");
+    var ButtonPlus = document.createElement("button");
+    ButtonPlus.setAttribute("click", "submit");
+    ButtonPlus.textContent = "+";
+    ButtonPlus.classList.add("button");
+    // ButtonPlus.style.width = "20px";
+    // ButtonPlus.style.height = "20px";
+    // ButtonPlus.style.textAlign ="center"
+    cryptoBtns.appendChild(ButtonPlus);
+    cryptoData.appendChild(cryptoBtns);
 
-    marketListEl.appendChild(table);
+    cryptoTable.appendChild(cryptoData);
+
+    marketListEl.appendChild(cryptoTable);
   }
+};
+
+var topFive = function () {
+  cryptoBtns.addEventListener("click", topFive);
+  localStorage.setItem("currentMarket", JSON.stringify(currentMarket));
 };
